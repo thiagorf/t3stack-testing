e54e274d230b4d37a46043b81b8424a5f76cfa50
thiagorf <trfarciroli@gmail.com>
2023-04-24 16:29:19 -0300

    create post loading state + error toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,15 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-        <Component {...pageProps} />
+      <Toaster position="bottom-center" />
+      <Component {...pageProps} />
     </ClerkProvider>
-    )
+  );
 };
 
 export default api.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,14 @@ const CreatePostWizard = () => {
       setInput("");
       void ctx.post.getAll.invalidate();
     },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later.");
+      }
+    },
   });
 
   if (!user) return null;
@@ -35,9 +43,24 @@ const CreatePostWizard = () => {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if ((e.key = "enter")) {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting}
       />
-      <button onClick={() => mutate({ content: input })}>Send</button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Send</button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
@@ -47,8 +70,9 @@ type PostWithUser = RouterOutputs["post"]["getAll"][number];
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
